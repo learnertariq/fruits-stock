25c0c8228c6b4b99a8c3d6dc8e907de8b1551181
Tariqul Islam <usukuk@gmail.com>
2022-04-29 16:54:50 +0600

    FruitCard styles and imgbb upload

--- src/components/Home/InventoryItemsHome/FruitsCard/FruitsCard.jsx
+++ src/components/Home/InventoryItemsHome/FruitsCard/FruitsCard.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import "./FruitsCard.css";
 
-const FruitsCard = ({ name, price, image }) => {
+const FruitsCard = ({ name, price, image, desc, quantity, supplier }) => {
   return (
     <div className="fruit-card">
-      <div className="row row-cols-2 bg-light rounded align-items-center">
-        <div className="col p-3">
+      <div className="row row-cols-2 bg-light rounded shadow mx-0">
+        <div className="col p-3 d-flex flex-column justify-content-between">
           <div className="">
-            <h6 className="fw-light mb-0">Top sales</h6>
+            <h6 className="fw-light mb-0">
+              Stock left:{" "}
+              <span className="fw-bold text-success">{quantity}</span>
+            </h6>
             <h3 className="fs-1">{name}</h3>
           </div>
-          <div className="mt-5">
-            <h6 className="fw-light mb-0">Weekend Discount</h6>
-            <h3 className="text-danger">${price}</h3>
+          <div className="">
+            <h3 className="text-danger fw-bold">${price}</h3>
+            <h6 className="fw-light mb-0 text-muted supplier-text">
+              Supplier: {supplier}
+            </h6>
+            <button className="fruit-update-btn btn btn-danger fw-bold text-uppercase px-4 py-1 mt-2">
+              Update
+            </button>
           </div>
         </div>
-        <div className="col pe-0">
+        <div className="col pe-0 d-flex flex-column justify-content-between">
           <img className="w-100" src={image} alt="" />
+          <p className="mb-1 fruit-desc pb-3">{desc}</p>
         </div>
       </div>
     </div>
--- src/components/Home/InventoryItemsHome/InventoryItemsHome.jsx
+++ src/components/Home/InventoryItemsHome/InventoryItemsHome.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 import FruitsCard from "./FruitsCard/FruitsCard";
 import "./InventoryItemsHome.css";
-import img1 from "../../../assets/fruits/1.webp";
+
+const data = [
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/qmHyT77/grape.webp",
+  },
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/7kbWJhj/date.webp",
+  },
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/fY2nH6Y/banana.webp",
+  },
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/yPyV0cL/coconut.webp",
+  },
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/y50Lpsw/guava.webp",
+  },
+  {
+    name: "Name",
+    quantity: 4,
+    supplier: "supplier company",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur",
+    img: "https://i.ibb.co/2NL2JSw/lichee.webp",
+  },
+];
 
 const InventoryItemsHome = () => {
   return (
@@ -9,16 +59,19 @@ const InventoryItemsHome = () => {
       <h1 className="inventory-heading mx-auto px-5 py-2 bg-warning mb-3">
         Fruits Stock
       </h1>
-      <div className="fruits-container row g-4">
-        <div className="col">
-          <FruitsCard name="Grapes" price="40" image={img1} />
-        </div>
-        <div className="col">
-          <FruitsCard name="Grapes" price="40" image={img1} />
-        </div>
-        <div className="col">
-          <FruitsCard name="Grapes" price="40" image={img1} />
-        </div>
+      <div className="fruits-container row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
+        {data.map((f, i) => (
+          <div key={i} className="col">
+            <FruitsCard
+              name="Grapes"
+              price="40"
+              image={f.img}
+              desc={f.desc}
+              quantity={f.quantity}
+              supplier={f.supplier}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
