17fc35d86ae1b81bcbd89bca97d46e1591fcd145
Tariqul Islam <usukuk@gmail.com>
2022-05-01 13:22:56 +0600

    decrease fruit quantity one by one with patch req

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,91 @@
-import React from 'react';
-import './ProductDetails.css'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+import "./ProductDetails.css";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const ProductDetails = () => {
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    const getProduct = async () => {
+      const res = await axios.get(`http://localhost:5000/fruits/${id}`);
+      console.log(res);
+      setProduct(res.data);
+    };
+    getProduct();
+  }, []);
+
+  const reduceQuantity = async () => {
+    const res = await axios.patch(`http://localhost:5000/fruits/${id}`, {
+      quantity: product.quantity - 1,
+    });
+    setProduct(res.data);
+  };
+
   return (
-    <div>
+    <section className="container">
       <h1>Product Details</h1>
-    </div>
+      <article className="product-details">
+        <div className="header d-flex justify-content-center mb-3">
+          <img
+            className="product-details-img bg-light"
+            src={product.img}
+            alt=""
+          />
+        </div>
+        <div className="main">
+          <div className="main-header row">
+            <div className="col col-8">
+              <h2>{product.name}</h2>
+              <h6 className="text-muted">{product.supplier}</h6>
+            </div>
+            <div className="col col-4 d-flex flex-column align-items-center">
+              <h6 className="mb-0">Stock left:</h6>
+              <span className="text-danger">🢃</span>
+              <span className="fs-3 fw-bold text-success">
+                {product.quantity}
+              </span>
+            </div>
+          </div>
+          <div className="sub-nav mt-4 mb-2 d-flex justify-content-between gap-2">
+            <Button variant="success" className="border border-success">
+              Description
+            </Button>
+            <Button
+              variant="outline"
+              className="border border-success opacity-75"
+            >
+              Nutrition
+            </Button>
+            <Button
+              variant="outline"
+              className="border border-success opacity-75"
+            >
+              Reviews
+            </Button>
+          </div>
+          <p className="desc">{product.desc}</p>
+          <div className="sub-nav mt-4 d-flex justify-content-between gap-2">
+            <Button variant="success" className="">
+              Ripe
+            </Button>
+            <Button variant="outline" className="border border-success">
+              Green
+            </Button>
+          </div>
+          <hr />
+          <div className="footer d-flex justify-content-between gap-2">
+            <h2 className="text-danger mb-0">${product.price}</h2>
+            <Button onClick={reduceQuantity} variant="danger" className="">
+              Delivered
+            </Button>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 };
 
