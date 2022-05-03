23abb89124cf936624a539dc044e0df64ee2f4c7
Tariqul Islam <usukuk@gmail.com>
2022-05-03 19:48:56 +0600

    fix footer same link text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,20 +18,6 @@ const Footer = () => {
             <div className="row row-cols-1 row-cols-md-3 g-3">
               <div className="col d-flex flex-column">
                 <div className="aritcle__header d-flex justify-content-between">
-                  <h3 className="footer__heading text-uppercase">Products</h3>
-                </div>
-                <a className="footer__link" href="#">
-                  Italian fruits
-                </a>
-                <a className="footer__link" href="#">
-                  Rajshahi fruits
-                </a>
-                <a className="footer__link" href="#">
-                  Chapai fruits
-                </a>
-              </div>
-              <div className="col d-flex flex-column">
-                <div className="aritcle__header d-flex justify-content-between">
                   <h3 className="footer__heading fs-5 text-uppercase">
                     Products
                   </h3>
@@ -48,18 +34,32 @@ const Footer = () => {
               </div>
               <div className="col d-flex flex-column">
                 <div className="aritcle__header d-flex justify-content-between">
+                  <h3 className="footer__heading text-uppercase">Company</h3>
+                </div>
+                <a className="footer__link" href="#">
+                  About
+                </a>
+                <a className="footer__link" href="#">
+                  Affiliates
+                </a>
+                <a className="footer__link" href="#">
+                  Blogs
+                </a>
+              </div>
+              <div className="col d-flex flex-column">
+                <div className="aritcle__header d-flex justify-content-between">
                   <h3 className="footer__heading fs-5 text-uppercase">
-                    Products
+                    Support
                   </h3>
                 </div>
                 <a className="footer__link" href="#">
-                  Italian fruits
+                  Contact
                 </a>
                 <a className="footer__link" href="#">
-                  Rajshahi fruits
+                  Knoledge base
                 </a>
                 <a className="footer__link" href="#">
-                  Chapai fruits
+                  FAQ
                 </a>
               </div>
             </div>
