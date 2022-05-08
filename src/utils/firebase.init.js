171853296eee803f34c67e7f683f43ea55f7063e
Tariqul Islam <usukuk@gmail.com>
2022-05-08 23:43:40 +0600

    hide secret info

--- src/utils/firebase.init.js
+++ src/utils/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7CQ2OfejTtoTu2Jnqqeq3Vz-Ei4T_Rqk",
-  authDomain: "fruits-stock-93d52.firebaseapp.com",
-  projectId: "fruits-stock-93d52",
-  storageBucket: "fruits-stock-93d52.appspot.com",
-  messagingSenderId: "914835548923",
-  appId: "1:914835548923:web:a4c898c9b0781a67791e5e",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AD,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_MSID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
